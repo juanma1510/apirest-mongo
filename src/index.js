8ab1c0eae868f2b4fc886a7264577030ff214994
juanma1510 <jmgarca4@misena.edu.co>
2023-11-21 20:24:01 -0500

    project finished, comming soon unitary test

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const productRoutes = require('./routes/product')
+
+const app = express();
+const port = process.env.PORT || 9000;  
+
+//middleware
+app.use(express.json());
+app.use('/api', productRoutes);
+
+//routes
+app.get('/', (req, res) =>{
+    res.send('Welcome to my API');
+})
+
+//mongodb connection
+mongoose
+.connect(process.env.MONGODB_URI)
+.then(() => console.log('connected to mongodb atlas'))
+.catch((error) => console.error(error));
+
+app.listen(port, () => console.log('server listening on port', port))
